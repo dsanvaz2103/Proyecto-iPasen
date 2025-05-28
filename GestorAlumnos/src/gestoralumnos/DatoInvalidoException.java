c98e684bba87f0d674167bc74a6b7cac7ebac227
Alejandro González <152494270+alego128@users.noreply.github.com>
2025-05-28 21:50:47 +0200

    Delete GestorAlumnos directory

--- GestorAlumnos/src/gestoralumnos/DatoInvalidoException.java
+++
@@ -1,5 +0,0 @@
-public class DatoInvalidoException extends Exception {
-    public DatoInvalidoException(String mensaje) {
-        super(mensaje);
-    }
-}
--- GestorAlumnos/src/gestoralumnos/DniDuplicadoException.java
+++
@@ -1,5 +0,0 @@
-public class DniDuplicadoException extends Exception {
-    public DniDuplicadoException(String mensaje) {
-        super(mensaje);
-    }
-}
